e05f8fbc976f4f652b5e413ea7aec794c4eecfb7
Khanh Linh Tran Lara <tklinh1072002@gmail.com>
2024-03-14 01:18:03 +0000

    create write_to_json_file() to avoid duplicated codes

--- unique_words.py
+++ unique_words.py
@@ -12,6 +12,21 @@ def format_json(dict):
         pairs.append(f'  "{k}": {json.dumps(v, separators=(",", ":"))}')
     return '{\n' + ',\n'.join(pairs) + '\n}'
 
+def write_to_json_file(file_path, data_to_update: dict):    
+    # Read existing JSON content
+    try:
+        with open(file_path, 'r') as json_file:
+            existing_data = json.load(json_file)
+    except Exception:
+        existing_data = {}  # If file doesn't exist, initialize with empty dictionary
+        
+    # Update existing data with new data
+    existing_data.update(data_to_update)
+    
+    # Write the dictionary to a JSON files
+    with open(file_path, 'w') as json_file:
+        json_file.write(format_json(existing_data))
+
 def extract_single_words(audio_prompt_sentences, unique_words_list = []):
     for sentence in audio_prompt_sentences:
         sentence = sentence.split()
@@ -61,20 +76,7 @@ def count_appearances(audio_prompt_sentences, unique_words_list):
         
     # Define json file path
     file_path = 'words_occurrences.json'
-    
-    # Read existing JSON content
-    try:
-        with open(file_path, 'r') as json_file:
-            existing_data = json.load(json_file)
-    except Exception:
-        existing_data = {}  # If file doesn't exist, initialize with empty dictionary
-        
-    # Update existing data with new data
-    existing_data.update(ordered_word_count_dict)
-    
-    # Write the dictionary to a JSON files
-    with open(file_path, 'w') as json_file:
-        json_file.write(format_json(existing_data))
+    write_to_json_file(file_path, ordered_word_count_dict)
 
 def parse_to_json(unique_words_list, pronunciation_dict, phonemes_list):
     '''
@@ -104,23 +106,10 @@ def parse_to_json(unique_words_list, pronunciation_dict, phonemes_list):
         
     # Define json file path
     file_path = 'words_to_alternative_phonemes.json'
-    
-    # Read existing JSON content
-    try:
-        with open(file_path, 'r') as json_file:
-            existing_data = json.load(json_file)
-    except Exception:
-        existing_data = {}  # If file doesn't exist, initialize with empty dictionary
+    write_to_json_file(file_path, word_dict)
 
-    # Update existing data with new data
-    existing_data.update(word_dict)
-    
-    # Write the dictionary to a JSON files
-    with open(file_path, 'w') as json_file:
-        json_file.write(format_json(existing_data))
-
-# Retrieve unique alternative phonemes used for the script words
-def retrieve_alt_phonemes(unique_phonemes = set()):
+# deduplicate alternative phonemes used for the script words
+def deduplicate_alt_phonemes(unique_phonemes = set()):
     '''
     From words-phonemes dictionary in JSON file, filter unique phonemes
     Store unique phonemes in a list
@@ -145,7 +134,7 @@ def bind_phonemes_words(phonemes_list, pronunciation_dict, phoneme_words_dict={}
     for i in range(len(phonemes_list)):
             corr_words = find_word(phonemes_list[i], pronunciation_dict)
             phoneme_words_dict[phonemes_list[i]] = [[corr_word] for corr_word in corr_words]
-            print(f"On {i+1}th phoneme")
+            #print(f"On {i+1}th phoneme")
     return phoneme_words_dict    
 
 def parse_phonemes_words_dict(phoneme_words_dict):
@@ -154,19 +143,7 @@ def parse_phonemes_words_dict(phoneme_words_dict):
     '''
     # Define json file path
     file_path = 'phonemes_to_corr_words.json'
-    # Read existing JSON content
-    try:
-        with open(file_path, 'r') as json_file:
-            existing_data = json.load(json_file)
-    except Exception:
-        existing_data = {}  # If file doesn't exist, initialize with empty dictionary
-
-    # Update existing data with new data
-    existing_data.update(phoneme_words_dict)
-    
-    # Write the dictionary to a JSON files
-    with open(file_path, 'w') as json_file:
-        json_file.write(format_json(existing_data))
+    write_to_json_file(file_path, phoneme_words_dict)
         
 def main():
     #######
@@ -187,10 +164,10 @@ def main():
     #parse_to_json(unique_words_list, pronunciation_dict, phonemes_list)
     
     ######
-    # Retrieve unique phonemes from JSON file
-    # unique_phoneme_list = retrieve_alt_phonemes()
-    # phoneme_words_dict = bind_phonemes_words(unique_phoneme_list, pronunciation_dict)
-    # parse_phonemes_words_dict(phoneme_words_dict)
+    # Get, parse unique phonemes and corr. words to JSON file
+    unique_phoneme_list = deduplicate_alt_phonemes()
+    phoneme_words_dict = bind_phonemes_words(unique_phoneme_list, pronunciation_dict)
+    parse_phonemes_words_dict(phoneme_words_dict)
     
     
 if __name__ == "__main__":
